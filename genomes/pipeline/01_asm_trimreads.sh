bf108d2cfbbe33769c8f70dceeab32d241cafaf6
Jason Stajich <jasonstajich.phd@gmail.com>
2018-07-30 16:44:32 -0700

    some steps revisited - still need to cleanup again

--- genomes/pipeline/01_asm_trimreads.sh
+++ genomes/pipeline/01_asm_trimreads.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#SBATCH --nodes 1 --ntasks 2 --mem 8G -J trimReads --out logs/trim.%a.log
+#SBATCH --nodes 1 --ntasks 16 --mem 24G -J trimReads --out logs/trim.%a.log
 
 module load trimmomatic
+module load samtools/1.8
+module load bwa/0.7.17
+module load bowtie2
+module load bedtools
+module load BBMap
 SAMPLES=samples.csv
 CPU=$SLURM_CPUS_ON_NODE
 if [ ! $CPU ]; then
@@ -17,6 +22,25 @@ if [ ! $N ]; then
         exit
     fi
 fi
+
+TMPDIR=tmp
+mkdir -p $TMPDIR
+if [ ! -f $TMPDIR/vectors.fa ]; then
+    pushd $TMPDIR
+    wget -c --tries=0 --read-timeout=20 ftp://ftp.ncbi.nlm.nih.gov/genomes/Viruses/enterobacteria_phage_phix174_sensu_lato_uid14015/NC_001422.fna
+    wget -c  --tries=0 --read-timeout=20 ftp://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec
+    # common lab contaminant
+    curl 'https://www.ebi.ac.uk/ena/data/view/CP011305.1%26display%3Dfasta' > CP011305.fna
+    curl 'https://www.ebi.ac.uk/ena/data/view/CP011306.1%26display%3Dfasta' > CP011306.fna
+    curl 'https://www.ebi.ac.uk/ena/data/view/CP015612.1%26display%3Dfasta' > CP015612.fna
+    curl 'https://www.ebi.ac.uk/ena/data/view/CP016756.1%26display%3Dfasta' > CP016756.fna
+    curl 'https://www.ebi.ac.uk/ena/data/view/CP022053.1%26display%3Dfasta' > CP022053.fna
+    cat *.fna UniVec > vectors.fa
+    bwa index -p vectors vectors.fa
+    bowtie2-build --threads $CPU vectors.fa vectors
+    popd
+fi
+dir=clean_reads
 ADT=$(dirname $TRIMMOMATIC)"/adapters/TruSeq3-PE.fa"
 echo $ADAPTOR
 IFS=,
@@ -24,11 +48,40 @@ sed -n ${N}p $SAMPLES | while read STRAIN LEFT RIGHT
 do
     echo "$STRAIN $LEFT $RIGHT"
     base=$(basename $LEFT _1.fq.gz)
-    dir=$(dirname $LEFT)
     if [ ! -f $dir/${base}_1.trim.fq.gz ]; then
        java -jar $TRIMMOMATIC PE -phred33 $LEFT $RIGHT \
             $dir/${base}_1.trim.fq.gz $dir/${base}_1.unpaired_trim.fq.gz \
             $dir/${base}_2.trim.fq.gz $dir/${base}_2.unpaired_trim.fq.gz \
             ILLUMINACLIP:$ADT:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:75
+    fi
+    if [ ! -f $dir/${base}.clean_bt.1.fq.gz ]; then
+      bowtie2 -x $TMPDIR/vectors -p $CPU -q -1 $dir/${base}_1.trim.fq.gz -2 $dir/${base}_2.trim.fq.gz --very-sensitive --un-conc-gz $dir/${base}.clean_bt.fq.gz > /dev/null 2>&1
+    fi
+    if [ ! -f $dir/${base}_1.clean.fq ]; then
+
+#       if [ ! -f $TMPDIR/${base}.vectors_unmapped_bbmap_sort.bam ]; then
+#            if [ ! -f $TMPDIR/${base}.vectors_bbmap.sam ]; then
+#             bbmap.sh ref=$TMPDIR/vectors.fa in1=$dir/${base}_1.trim.fq.gz in2=$dir/${base}_2.trim.fq.gz out=$TMPDIR/${base}.vectors_bbmap.sam nodisk
+#            fi
+#            samtools view --threads $CPU -b  $TMPDIR/${base}.vectors_bbmap.sam >  $TMPDIR/${base}.vectors_bbmap.bam
+#            samtools view --threads $CPU -b -f 12 $TMPDIR/${base}.vectors_bbmap.bam > $TMPDIR/${base}.vectors_unmapped_bbmap.bam
+#            samtools sort --threads $CPU -n $TMPDIR/${base}.vectors_unmapped_bbmap.bam > $TMPDIR/${base}.vectors_unmapped_bbmap_sort.bam
+#            unlink $TMPDIR/${base}.vectors_bbmap.sam
+#        fi
+       if [ ! -f $TMPDIR/${base}.vectors_unmapped_sort.bam ]; then
+            bwa mem -t $CPU $TMPDIR/vectors $dir/${base}_1.trim.fq.gz $dir/${base}_2.trim.fq.gz > $TMPDIR/${base}.vectors_map.sam
+            samtools view --threads $CPU -b  $TMPDIR/${base}.vectors_map.sam >  $TMPDIR/${base}.vectors_map.bam
+            samtools view --threads $CPU -b -f 12 $TMPDIR/${base}.vectors_map.bam > $TMPDIR/${base}.vectors_unmapped.bam
+            samtools sort --threads $CPU -n $TMPDIR/${base}.vectors_unmapped.bam > $TMPDIR/${base}.vectors_unmapped_sort.bam
+            unlink $TMPDIR/${base}.vectors_map.sam
+        fi   
+#        if [  ! -f $dir/${base}_1.clean_bbmap.fq.gz ]; then
+#            bedtools bamtofastq -i $TMPDIR/${base}.vectors_unmapped_bbmap_sort.bam -fq $dir/${base}_1.clean_bbmap.fq -fq2 $dir/${base}_2.clean_bbmap.fq
+#            pigz $dir/${base}_1.clean_bbmap.fq $dir/${base}_2.clean_bbmap.fq
+#        fi
+        if [ ! -f $dir/${base}_1.clean.fq.gz ]; then
+            bedtools bamtofastq -i $TMPDIR/${base}.vectors_unmapped_sort.bam -fq $dir/${base}_1.clean.fq -fq2 $dir/${base}_2.clean.fq
+            pigz $dir/${base}_1.clean.fq $dir/${base}_2.clean.fq
+        fi
     fi
 done
--- genomes/pipeline/03_asm_cleanasm.sh
+++ genomes/pipeline/03_asm_cleanasm.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH -p batch --time 2-0:00:00 --ntasks 2 --nodes 1 --mem 8G --out logs/clean.%a.log
-module load funannotate/git-live
 module load ncbi-blast/2.7.1+
-module load minimap2
-INDIR=genomes
-OUTDIR=genomes_clean
+module load python/3
+INDIR=asm
+OUTDIR=asm_clean
 MINLEN=500
-VECTORDB=/srv/projects/db/UniVec/UniVec
-CLEANVEV=`pwd`"/autovectorscreen/scripts/clean_vect_blastn.py"
+DB=/srv/projects/db/UniVec/UniVec
+CLEANVEC=`pwd`"../autovectorscreen/scripts/clean_vect_blastn.py"
 
 CPU=1
 if [ $SLURM_CPUS_ON_NODE ]; then
@@ -29,32 +28,28 @@ if [ $N -gt $MAX ]; then
     exit
 fi
 IFS=,
-sed -n ${N}p $SAMPFILE | while read ProjID JGISample JGIProjName JGIBarcode SubPhyla Species Strain
+sed -n ${N}p $SAMPFILE | while read STRAIN LEFT RIGHT
 do
 #currently species column also contains strain info so no need to concatenate
- name=$(echo "$Species" | perl -p -e 'chomp; s/\s+/_/g')
- if [ ! -f $OUTDIR/$name.SPAdes_clean.fasta ]; then
-    mkdir -p $name.$$
-    pushd $name.$$
+ if [ ! -f $OUTDIR/$STRAIN.SPAdes_clean.fasta ]; then
+    mkdir -p $STRAIN.$$
+    pushd $STRAIN.$$
     # if > XX we should probably remove
-    perl $SCRIPTS/filter_db_by_seqlen.pl -l $MINLEN ../$INDIR/$name.SPAdes.fasta > $name.trunc.fasta
+    perl $SCRIPTS/filter_db_by_seqlen.pl -l $MINLEN ../$INDIR/$STRAIN/scaffolds.fasta > $STRAIN.trunc.fasta
 
-#    blastn -reward 1 -penalty -5 -gapopen 3 -gapextend 3 -dust yes -soft_masking true -evalue 700 -searchsp 1750000000000 -db $VECTORDB \
-#         -query $name.trunc.fasta -out $name.vecscreen -num_threads $CPU -outfmt 6
-#    module switch python/3
-#    python $CLEANVEC $name.trunc.fasta $name.vecscreen
-#    module switch python/2.7.5
+    blastn -task blastn -reward 1 -penalty -5 -gapopen 3 -gapextend 3 -dust yes -soft_masking true -evalue 700 -searchsp 1750000000000 \
+        -db $DB -outfmt 6 -num_threads $CPU -query $STRAIN.trunc.fasta -out $STRAIN.vecscreen.blastn
 
-    COUNT=$(grep -c '>' $name.trunc.fasta)
-    echo "COUNT is $COUNT"
-    if [ "$COUNT" -gt "3000" ]; then
-      mv $name.trunc.fasta ../$OUTDIR/$name.SPAdes_clean.fasta 
-    else
-        funannotate clean -i $name.trunc.fasta -o ../$OUTDIR/$name.SPAdes_clean.fasta
-        rm $name.trunc.fasta
-    fi
-    popd
-    rmdir $name.$$
+#    COUNT=$(grep -c '>' $name.trunc.fasta)
+#    echo "COUNT is $COUNT"
+#    if [ "$COUNT" -gt "3000" ]; then
+#      mv $name.trunc.fasta ../$OUTDIR/$name.SPAdes_clean.fasta 
+#    else
+#        funannotate clean -i $name.trunc.fasta -o ../$OUTDIR/$name.SPAdes_clean.fasta
+#        rm $name.trunc.fasta
+#    fi
+#    popd
+#    rmdir $name.$$
  else 
      echo "Already processed and created $OUTDIR/$name.SPAdes_clean.fasta"
  fi
